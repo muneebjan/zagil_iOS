1e4139202a5c0deafb8c2836805f66c98c3b24aa
Muneeb jan <muneebahsan07@gmail.com>
2019-12-26 16:21:00 +0500

    Initial Commit

---
+++ Zagil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zagil
+//
+//  Created by Apple on 26/12/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
